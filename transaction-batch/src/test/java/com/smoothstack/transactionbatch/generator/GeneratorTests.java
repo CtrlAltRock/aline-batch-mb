1d2af0986fa56f46db54bb21e9f70561cb3cfd04
mbombsstack <mark.bomberry@smoothstack.com>
2022-04-26 16:42:12 -0500

    NRVNA-20 Testing for the user generator

---
+++ transaction-batch/src/test/java/com/smoothstack/transactionbatch/generator/GeneratorTests.java
@@ -0,0 +1,34 @@
+package com.smoothstack.transactionbatch.generator;
+
+import com.smoothstack.transactionbatch.model.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class GeneratorTests {
+    private final UserGenerator userGenerator = UserGenerator.getInstance();
+    private final CardGenerator cardGenerator = CardGenerator.getInstance();
+    private final MerchantGenerator merchantGenerator = MerchantGenerator.getInstance();
+    
+    @Test
+    public void correctUserGeneration() {
+        Optional<UserBase> testUser = userGenerator.generateUser(1);
+
+        assertTrue(testUser.isPresent());
+
+        UserBase user = testUser.get();
+
+        assertEquals(1L, user.getId());
+        assertNotNull(user.getFirstName());
+        assertNotNull(user.getMiddleName());
+        assertNotNull(user.getDateOfBirth());
+        assertNotNull(user.getPhone());
+    }
+
+    
+}
